7c5b17d6f22f256063c211f42406e1fa42838a56
Anton Serra <anton@Mac-mini-de-Anton.local>
2016-06-04 22:53:52 +0200

    add windfarm entity

---
+++ src/AppBundle/Repository/WindfarmRepository.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace AppBundle\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+/**
+ * Class WindfarmRepository
+ *
+ * @category Repository
+ * @package  AppBundle\Repository
+ * @author   Anton Serra <aserratorta@gmail.com>
+ */
+class WindfarmRepository extends EntityRepository
+{
+
+}
