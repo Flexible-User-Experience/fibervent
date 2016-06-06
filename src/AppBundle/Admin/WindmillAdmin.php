97b89a0008cd2526a0e35a89abafb0564a09a393
Anton Serra <anton@Mac-mini-de-Anton.local>
2016-06-06 23:10:31 +0200

    add windmill admin

---
+++ src/AppBundle/Admin/WindmillAdmin.php
@@ -0,0 +1,205 @@
+<?php
+
+namespace AppBundle\Admin;
+
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+
+/**
+ * Class WindmillAdmin
+ *
+ * @category Admin
+ * @package  AppBundle\Admin
+ * @author   Anton Serra <aserratorta@gmail.com>
+ */
+class WindmillAdmin extends AbstractBaseAdmin
+{
+    protected $classnameLabel = 'Windmill';
+    protected $baseRoutePattern = 'windfarms/windmill';
+    protected $datagridValues = array(
+        '_sort_by'    => 'code',
+        '_sort_order' => 'asc',
+    );
+
+    /**
+     * @param FormMapper $formMapper
+     */
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->with('General', $this->getFormMdSuccessBoxArray(7))
+            ->add(
+                'code',
+                null,
+                array(
+                    'label' => 'Codi',
+                )
+            )
+            ->add(
+                'windfarm',
+                null,
+                array(
+                    'label' => 'Parc Eòlic',
+                )
+            )
+//            ->add(
+//                'state',
+//                null,
+//                array(
+//                    'label' => 'Província',
+//                )
+//            )
+            ->add(
+                'windfarm.manager',
+                null,
+                array(
+                    'label' => 'Administrador',
+                )
+            )
+            ->end()
+            ->with('Controls', $this->getFormMdSuccessBoxArray(5))
+            ->add(
+                'enabled',
+                CheckboxType::class,
+                array(
+                    'label'    => 'Actiu',
+                    'required' => false,
+                )
+            )
+            ->add(
+                'gpsLongitude',
+                null,
+                array(
+                    'label' => 'Longitud',
+                )
+            )
+            ->add(
+                'gpsLatitude',
+                null,
+                array(
+                    'label' => 'Latitud',
+                )
+            )
+            ->end();
+    }
+
+    /**
+     * @param DatagridMapper $datagridMapper
+     */
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add(
+                'code',
+                null,
+                array(
+                    'label' => 'Codi',
+                )
+            )
+            ->add(
+                'windfarm',
+                null,
+                array(
+                    'label' => 'Parc Eòlic',
+                )
+            )
+//            ->add(
+//                'windfarm.manager',
+//                null,
+//                array(
+//                    'label' => 'Administrador',
+//                )
+//            )
+//            ->add(
+//                'windfarm.customer',
+//                null,
+//                array(
+//                    'label' => 'Client',
+//                )
+//            )
+//            ->add(
+//                'state',
+//                null,
+//                array(
+//                    'label' => 'Província',
+//                )
+//            )
+//            ->add(
+//                'state.country',
+//                null,
+//                array(
+//                    'label' => 'País',
+//                )
+//            )
+            ->add(
+                'enabled',
+                null,
+                array(
+                    'label'    => 'Actiu',
+                    'editable' => true,
+                )
+            );
+    }
+
+    /**
+     * @param ListMapper $listMapper
+     */
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        unset($this->listModes['mosaic']);
+        $listMapper
+            ->add(
+                'code',
+                null,
+                array(
+                    'label'    => 'Codi',
+                    'editable' => true,
+                )
+            )
+            ->add(
+                'windfarm',
+                null,
+                array(
+                    'label'    => 'Parc Eòlic',
+                    'editable' => true,
+                )
+            )
+            ->add(
+                'gpsLongitude',
+                null,
+                array(
+                    'label' => 'Longitud',
+                    'editable' => true,
+                )
+            )
+            ->add(
+                'gpsLatitude',
+                null,
+                array(
+                    'label' => 'Latitud',
+                    'editable' => true,
+                )
+            )
+            ->add(
+                'enabled',
+                null,
+                array(
+                    'label'    => 'Actiu',
+                    'editable' => true,
+                )
+            )
+            ->add(
+                '_action',
+                'actions',
+                array(
+                    'label'   => 'Accions',
+                    'actions' => array(
+                        'edit'   => array('template' => '::Admin/Buttons/list__action_edit_button.html.twig'),
+                        'delete' => array('template' => '::Admin/Buttons/list__action_delete_button.html.twig'),
+                    )
+                )
+            );
+    }
+}
