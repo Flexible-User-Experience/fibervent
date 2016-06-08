081dd676a23289e388a16ca38cf7815922125a50
Anton Serra <aserratorta@gmail.com>
2016-06-08 17:44:52 +0200

    add AuditWindmillBlade entity

---
+++ src/AppBundle/Repository/AuditWindmillBladeRepository.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace AppBundle\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+/**
+ * Class AuditWindmillBladeRepository
+ *
+ * @category Repository
+ * @package  AppBundle\Repository
+ * @author   Anton Serra <aserratorta@gmail.com>
+ */
+class AuditWindmillBladeRepository extends EntityRepository
+{
+
+}
