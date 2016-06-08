e94bcc57376dd1c8ac934d928e3f78a99f4f2bb1
Anton Serra <aserratorta@gmail.com>
2016-06-08 06:57:04 +0200

    add and conf BladeAdmin

---
+++ src/AppBundle/Admin/BladeAdmin.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace AppBundle\Admin;
+
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+
+/**
+ * Class BladeAdmin
+ *
+ * @category Admin
+ * @package  AppBundle\Admin
+ * @author   Anton Serra <aserratorta@gmail.com>
+ */
+class BladeAdmin extends AbstractBaseAdmin
+{
+    protected $classnameLabel = 'Blade';
+    protected $baseRoutePattern = 'windfarms/blade';
+    protected $datagridValues = array(
+        '_sort_by'    => 'model',
+        '_sort_order' => 'desc',
+    );
+
+    /**
+     * @param FormMapper $formMapper
+     */
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->with('General', $this->getFormMdSuccessBoxArray(7))
+            ->add(
+                'model',
+                null,
+                array(
+                    'label'    => 'Model',
+                    'required' => true,
+                )
+            )
+            ->add(
+                'length',
+                null,
+                array(
+                    'label'    => 'Longitud',
+                    'required' => true,
+                )
+            )
+            ->end()
+            ->with('Controls', $this->getFormMdSuccessBoxArray(5))
+            ->add(
+                'enabled',
+                CheckboxType::class,
+                array(
+                    'label'    => 'Actiu',
+                    'required' => false,
+                )
+            )
+            ->end();
+    }
+
+    /**
+     * @param DatagridMapper $datagridMapper
+     */
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add(
+                'model',
+                null,
+                array(
+                    'label'    => 'Model',
+                )
+            )
+            ->add(
+                'length',
+                null,
+                array(
+                    'label'    => 'Longitud',
+                )
+            )
+            ->add(
+                'enabled',
+                null,
+                array(
+                    'label'    => 'Actiu',
+                    'editable' => true,
+                )
+            );
+    }
+
+    /**
+     * @param ListMapper $listMapper
+     */
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        unset($this->listModes['mosaic']);
+        $listMapper
+            ->add(
+                'model',
+                null,
+                array(
+                    'label'    => 'Model',
+                    'editable' => true,
+                )
+            )
+            ->add(
+                'length',
+                null,
+                array(
+                    'label'    => 'Longitud',
+                    'editable' => true,
+                )
+            )
+            ->add(
+                '_action',
+                'actions',
+                array(
+                    'label'   => 'Accions',
+                    'actions' => array(
+                        'edit'   => array('template' => '::Admin/Buttons/list__action_edit_button.html.twig'),
+                        'delete' => array('template' => '::Admin/Buttons/list__action_delete_button.html.twig'),
+                    )
+                )
+            );
+    }
+}
