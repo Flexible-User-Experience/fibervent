a29f2b571e774eb2c6bb4440bb3e122c971ea343
Anton Serra <aserratorta@gmail.com>
2016-06-08 16:26:44 +0200

    add Audit entity

---
+++ src/AppBundle/Entity/Audit.php
@@ -0,0 +1,225 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * Audit
+ *
+ * @category Entity
+ * @package  AppBundle\Entity
+ * @author   Anton Serra <aserratorta@gmail.com>
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\AuditRepository")
+ */
+class Audit extends AbstractBase
+{
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    protected $beginDate;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    protected $endDate;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    protected $status;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", length=255)
+     */
+    private $type;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="text", length=4000, nullable=true)
+     */
+    private $tools;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="text", length=4000, nullable=true)
+     */
+    private $observations;
+
+    /**
+     * @var Windmill
+     *
+     * @ORM\ManyToOne(targetEntity="Windmill", inversedBy="audits")
+     */
+    private $windmill;
+
+    /**
+     *
+     *
+     * Methods
+     *
+     *
+     */
+
+    /**
+     * @return \DateTime
+     */
+    public function getBeginDate()
+    {
+        return $this->beginDate;
+    }
+
+    /**
+     * @param \DateTime $beginDate
+     *
+     * @return Audit
+     */
+    public function setBeginDate($beginDate)
+    {
+        $this->beginDate = $beginDate;
+
+        return $this;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getEndDate()
+    {
+        return $this->endDate;
+    }
+
+    /**
+     * @param \DateTime $endDate
+     *
+     * @return Audit
+     */
+    public function setEndDate($endDate)
+    {
+        $this->endDate = $endDate;
+
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * @param int $status
+     * @return Audit
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * @param string $type
+     *
+     * @return Audit
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTools()
+    {
+        return $this->tools;
+    }
+
+    /**
+     * @param string $tools
+     *
+     * @return Audit
+     */
+    public function setTools($tools)
+    {
+        $this->tools = $tools;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getObservations()
+    {
+        return $this->observations;
+    }
+
+    /**
+     * @param string $observations
+     *
+     * @return Audit
+     */
+    public function setObservations($observations)
+    {
+        $this->observations = $observations;
+
+        return $this;
+    }
+
+    /**
+     * @return Windmill
+     */
+    public function getWindmill()
+    {
+        return $this->windmill;
+    }
+
+    /**
+     * @param Windmill $windmill
+     *
+     * @return Audit
+     */
+    public function setWindmill($windmill)
+    {
+        $this->windmill = $windmill;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString()
+    {
+        return $this->getCode() ? $this->getCode() : '---';
+    }
+}
