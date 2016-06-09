416b9f74a3a18c767d1708d6c6e4f0eebcc41d42
Anton Serra <aserratorta@gmail.com>
2016-06-09 11:01:10 +0200

    add Damage entity

---
+++ src/AppBundle/Entity/Damage.php
@@ -0,0 +1,157 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use AppBundle\Entity\Traits\CodeTrait;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
+/**
+ * Damage
+ *
+ * @category Entity
+ * @package  AppBundle\Entity
+ * @author   Anton Serra <aserratorta@gmail.com>
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\DamageRepository")
+ * @UniqueEntity("code")
+ */
+class Damage extends AbstractBase
+{
+    use CodeTrait;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(type="integer")
+     */
+    protected $section;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", length=45, unique = true)
+     */
+    private $code;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", length=255)
+     */
+    private $description;
+
+    /**
+     * @var ArrayCollection
+     *
+     * @ORM\OneToMany(targetEntity="BladeDamages", mappedBy="damage")
+     */
+    private $bladeDamages;
+
+    /**
+     *
+     *
+     * Methods
+     *
+     *
+     */
+
+    /**
+     * Damage constructor.
+     */
+    public function __construct()
+    {
+        $this->bladeDamages = new ArrayCollection();
+    }
+
+    /**
+     * @return int
+     */
+    public function getSection()
+    {
+        return $this->section;
+    }
+
+    /**
+     * @param int $section
+     *
+     * @return Damage
+     */
+    public function setSection($section)
+    {
+        $this->section = $section;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCode()
+    {
+        return $this->code;
+    }
+
+    /**
+     * @param string $code
+     *
+     * @return Damage
+     */
+    public function setCode($code)
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string $description
+     *
+     * @return Damage
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * @return ArrayCollection
+     */
+    public function getBladeDamages()
+    {
+        return $this->bladeDamages;
+    }
+
+    /**
+     * @param ArrayCollection $bladeDamages
+     *
+     * @return Damage
+     */
+    public function setBladeDamages(ArrayCollection $bladeDamages)
+    {
+        $this->bladeDamages = $bladeDamages;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString()
+    {
+        return $this->getCode() ? $this->getCode() : '---';
+    }
+}
