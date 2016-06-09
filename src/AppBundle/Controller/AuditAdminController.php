0acbea3ff10c3faa0b9ae86438bded34ab009991
David Romaní <david@flux.cat>
2016-06-09 11:35:12 +0200

    add audit admin controller

---
+++ src/AppBundle/Controller/AuditAdminController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sonata\AdminBundle\Controller\CRUDController as Controller;
+
+/**
+ * Class AuditAdminController
+ *
+ * @category Controller
+ * @package  AppBundle\Controller
+ * @author   David Romaní <david@flux.cat>
+ */
+class AuditAdminController extends Controller
+{
+}
