485c2e5a42bb2fa50e130affba31200ff8988126
David Romaní <david@flux.cat>
2016-06-09 12:06:10 +0200

    enable pdf button again

--- src/AppBundle/Admin/AuditAdmin.php
+++ src/AppBundle/Admin/AuditAdmin.php
@@ -5,6 +5,7 @@ namespace AppBundle\Admin;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Route\RouteCollection;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
@@ -23,6 +24,18 @@ class AuditAdmin extends AbstractBaseAdmin
         '_sort_by'    => 'beginDate',
         '_sort_order' => 'desc',
     );
+
+    /**
+     * Configure route collection
+     *
+     * @param RouteCollection $collection
+     */
+    protected function configureRoutes(RouteCollection $collection)
+    {
+        parent::configureRoutes($collection);
+        $collection
+            ->add('pdf', $this->getRouterIdParameter() . '/pdf');
+    }
 
     /**
      * @param FormMapper $formMapper
@@ -200,6 +213,7 @@ class AuditAdmin extends AbstractBaseAdmin
                     'label'   => 'Accions',
                     'actions' => array(
                         'edit'   => array('template' => '::Admin/Buttons/list__action_edit_button.html.twig'),
+                        'pdf'    => array('template' => '::Admin/Buttons/list__action_pdf_button.html.twig'),
                         'delete' => array('template' => '::Admin/Buttons/list__action_delete_button.html.twig'),
                     )
                 )
--- src/AppBundle/Controller/AuditAdminController.php
+++ src/AppBundle/Controller/AuditAdminController.php
@@ -2,7 +2,12 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Audit;
 use Sonata\AdminBundle\Controller\CRUDController as Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 
 /**
  * Class AuditAdminController
@@ -13,4 +18,26 @@ use Sonata\AdminBundle\Controller\CRUDController as Controller;
  */
 class AuditAdminController extends Controller
 {
+    /**
+     * Export Audit in PDF format action
+     *
+     * @param int|string|null $id
+     * @param Request $request
+     *
+     * @return Response
+     * @throws NotFoundHttpException If the object does not exist
+     * @throws AccessDeniedHttpException If access is not granted
+     */
+    public function pdfAction($id = null, Request $request = null)
+    {
+//        $request = $this->resolveRequest($request);
+//        $id = $request->get($this->admin->getIdParameter());
+
+        /** @var Audit $object */
+        $object = $this->admin->getObject($id);
+
+        if (!$object) {
+            throw $this->createNotFoundException(sprintf('unable to find the object with id : %s', $id));
+        }
+    }
 }
