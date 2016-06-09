b6d28773c6fd6ee238c424e57514fa4064ad937e
Anton Serra <aserratorta@gmail.com>
2016-06-09 17:09:54 +0200

    add and configure DamageCategoryAdmin

---
+++ src/AppBundle/Admin/DamageCategoryAdmin.php
@@ -0,0 +1,173 @@
+<?php
+
+namespace AppBundle\Admin;
+
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+
+/**
+ * Class DamageCategoryAdmin
+ *
+ * @category Admin
+ * @package  AppBundle\Admin
+ * @author   Anton Serra <aserratorta@gmail.com>
+ */
+class DamageCategoryAdmin extends AbstractBaseAdmin
+{
+    protected $classnameLabel = 'Categories de Dany';
+    protected $baseRoutePattern = 'audits/damage-category';
+    protected $datagridValues = array(
+        '_sort_by'    => 'category',
+        '_sort_order' => 'asc',
+    );
+
+    /**
+     * @param FormMapper $formMapper
+     */
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->with('General', $this->getFormMdSuccessBoxArray(7))
+            ->add(
+                'category',
+                null,
+                array(
+                    'label'    => 'Categoria',
+                    'required' => true,
+                )
+            )
+            ->add(
+                'priority',
+                null,
+                array(
+                    'label'    => 'Prioritat',
+                    'required' => true,
+                )
+            )
+            ->add(
+                'description',
+                null,
+                array(
+                    'label'    => 'Descripció',
+                    'required' => true,
+                )
+            )
+            ->add(
+                'recommendedAction',
+                null,
+                array(
+                    'label'    => 'Acció Recomanada',
+                    'required' => true,
+                )
+            )
+            ->end()
+            ->with('Controls', $this->getFormMdSuccessBoxArray(5))
+            ->add(
+                'enabled',
+                CheckboxType::class,
+                array(
+                    'label'    => 'Actiu',
+                    'required' => false,
+                )
+            )
+            ->end();
+    }
+
+    /**
+     * @param DatagridMapper $datagridMapper
+     */
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add(
+                'category',
+                null,
+                array(
+                    'label'    => 'Categoria',
+                )
+            )
+            ->add(
+                'priority',
+                null,
+                array(
+                    'label'    => 'Prioritat',
+                )
+            )
+            ->add(
+                'description',
+                null,
+                array(
+                    'label'    => 'Descripció',
+                )
+            )
+            ->add(
+                'recommendedAction',
+                null,
+                array(
+                    'label'    => 'Acció Recomanada',
+                )
+            )
+            ->add(
+                'enabled',
+                null,
+                array(
+                    'label'    => 'Actiu',
+                    'editable' => true,
+                )
+            );
+    }
+
+    /**
+     * @param ListMapper $listMapper
+     */
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        unset($this->listModes['mosaic']);
+        $listMapper
+            ->add(
+                'category',
+                null,
+                array(
+                    'label'    => 'Categoria',
+                    'editable' => true,
+                )
+            )
+            ->add(
+                'priority',
+                null,
+                array(
+                    'label'    => 'Prioritat',
+                    'editable' => true,
+                )
+            )
+            ->add(
+                'description',
+                null,
+                array(
+                    'label'    => 'Descripció',
+                    'editable' => true,
+                )
+            )
+            ->add(
+                'recommendedAction',
+                null,
+                array(
+                    'label'    => 'Acció Recomanada',
+                    'editable' => true,
+                )
+            )
+            ->add(
+                '_action',
+                'actions',
+                array(
+                    'label'   => 'Accions',
+                    'actions' => array(
+                        'edit'   => array('template' => '::Admin/Buttons/list__action_edit_button.html.twig'),
+                        'delete' => array('template' => '::Admin/Buttons/list__action_delete_button.html.twig'),
+                    )
+                )
+            );
+    }
+}
