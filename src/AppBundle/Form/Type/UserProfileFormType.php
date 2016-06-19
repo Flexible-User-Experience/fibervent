7a5f031e20d1a19d282f9c52ea2082f987a84f4b
David Romaní <david@flux.cat>
2016-06-19 16:25:16 +0200

    fix https://trello.com/c/6sxTRnz2/50-improve-admin-user-profile III

--- src/AppBundle/Form/Type/UserProfileFormType.php
+++ src/AppBundle/Form/Type/UserProfileFormType.php
@@ -4,6 +4,7 @@ namespace AppBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -64,6 +65,14 @@ class UserProfileFormType extends AbstractType
                 TextType::class,
                 array(
                     'label'    => 'Telèfon',
+                    'required' => false,
+                )
+            )
+            ->add(
+                'imageFile',
+                FileType::class,
+                array(
+                    'label'    => ' ',
                     'required' => false,
                 )
             )
