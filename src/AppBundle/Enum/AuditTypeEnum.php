0952457abd973536d977e160c5cc8c15bd6c0b0a
David Romaní <david@flux.cat>
2016-08-23 16:12:18 +0200

    add audit type enum

---
+++ src/AppBundle/Enum/AuditTypeEnum.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace AppBundle\Enum;
+
+use AppBundle\Entity\Audit;
+
+/**
+ * Class AuditTypeEnum
+ *
+ * @category Enum
+ * @package  AppBundle\Enum
+ * @author   David Romaní <david@flux.cat>
+ */
+class AuditTypeEnum
+{
+    const GROUND = 0;
+    const ROPE   = 1;
+    const INSIDE = 2;
+
+    /**
+     * @return array
+     */
+    public static function getEnumArray()
+    {
+        return array(
+            self::GROUND => 'suelo (telescopio Fibervent)',
+            self::ROPE   => 'cuerdas',
+            self::INSIDE => 'interior pala',
+        );
+    }
+
+    /**
+     * @return array
+     */
+    public static function getLocalizedEnumArray()
+    {
+        return array(
+            self::GROUND => 'enum.audit_type.ground',
+            self::ROPE   => 'enum.audit_type.rope',
+            self::INSIDE => 'enum.audit_type.inside',
+        );
+    }
+
+    /**
+     * @param Audit $audit
+     *
+     * @return string
+     */
+    public static function getStringValue(Audit $audit)
+    {
+        return self::getEnumArray()[$audit->getType()];
+    }
+
+    /**
+     * @param Audit $audit
+     *
+     * @return string
+     */
+    public static function getStringLocalizedValue(Audit $audit)
+    {
+        return self::getLocalizedEnumArray()[$audit->getType()];
+    }
+}
