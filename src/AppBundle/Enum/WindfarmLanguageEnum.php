c85cd8004180ed68d477dfc6f1bd96111e86c85d
Anton Serra <aserratorta@gmail.com>
2016-10-04 22:54:30 +0200

    add WindfarmLanguageEnum https://trello.com/c/VPae8iC7/136-afegir-angles-als-excels

---
+++ src/AppBundle/Enum/WindfarmLanguageEnum.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace AppBundle\Enum;
+
+/**
+ * Class AuditLanguageEnum
+ *
+ * @category Enum
+ * @package  AppBundle\Enum
+ * @author   David Romaní <david@flux.cat>
+ */
+class WindfarmLanguageEnum extends AuditLanguageEnum
+{
+}
