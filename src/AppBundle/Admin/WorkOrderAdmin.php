318fd68132997726ee82c4030f92d2688c82139c
jordi.sort <jordi.sort@mirmit.com>
2019-08-29 16:40:01 +0200

    WorkOrderAdmin list view improved with its necessary tranlations added

--- src/AppBundle/Admin/WorkOrderAdmin.php
+++ src/AppBundle/Admin/WorkOrderAdmin.php
@@ -12,47 +12,143 @@ class WorkOrderAdmin extends AbstractAdmin
 {
     protected $classnameLabel = 'admin.workorder.title';
     protected $baseRoutePattern = 'workorders/workorder';
+    protected $datagridValues = array(
+        '_sort_by' => 'projectNumber',
+        '_sort_order' => 'desc',
+    );
 
+    /**
+     * @param DatagridMapper $datagridMapper
+     */
     protected function configureDatagridFilters(DatagridMapper $datagridMapper)
     {
         $datagridMapper
-            ->add('projectNumber')
-            ->add('isFromAudit')
-            ->add('certifyingCompanyName')
-            ->add('certifyingCompanyContactPerson')
-            ->add('certifyingCompanyPhone')
-            ->add('certifyingCompanyEmail')
-            ->add('repairAccessTypes')
-            ->add('id')
-            ->add('createdAt')
-            ->add('updatedAt')
-            ->add('enabled')
-            ->add('removedAt')
+            ->add('projectNumber',
+                null,
+                array(
+                    'label' => 'admin.work_order.project_number',
+                )
+            )
+            ->add(
+                'customer',
+                null,
+                array(
+                    'label' => 'admin.windfarm.customer',
+                    'sortable' => true,
+                    'sort_field_mapping' => array('fieldName' => 'name'),
+                    'sort_parent_association_mappings' => array(array('fieldName' => 'customer')),
+                )
+            )
+            ->add('isFromAudit',
+                null,
+                array(
+                    'label' => 'admin.work_order.is_from_audit',
+                )
+            )
+            ->add('certifyingCompanyName',
+                null,
+                array(
+                    'label' => 'admin.work_order.certifying_company_name',
+                )
+            )
+            ->add('certifyingCompanyContactPerson',
+                null,
+                array(
+                    'label' => 'admin.work_order.certifying_company_contact_person',
+                )
+            )
+            ->add('certifyingCompanyPhone',
+                null,
+                array(
+                    'label' => 'admin.work_order.certifying_company_phone',
+                )
+            )
+            ->add('certifyingCompanyEmail',
+                null,
+                array(
+                    'label' => 'admin.work_order.certifying_company_email',
+                )
+            )
+            ->add('repairAccessTypes',
+                null,
+                array(
+                    'label' => 'admin.work_order.repair_access_types',
+                )
+            )
         ;
     }
 
+    /**
+     * @param ListMapper $listMapper
+     */
     protected function configureListFields(ListMapper $listMapper)
     {
         $listMapper
-            ->add('projectNumber')
-            ->add('isFromAudit')
-            ->add('certifyingCompanyName')
-            ->add('certifyingCompanyContactPerson')
-            ->add('certifyingCompanyPhone')
-            ->add('certifyingCompanyEmail')
-            ->add('repairAccessTypes')
-            ->add('id')
-            ->add('createdAt')
-            ->add('updatedAt')
-            ->add('enabled')
-            ->add('removedAt')
-            ->add('_action', null, [
-                'actions' => [
-                    'show' => [],
-                    'edit' => [],
-                    'delete' => [],
-                ],
-            ])
+            ->add('projectNumber',
+                null,
+                array(
+                    'label' => 'admin.work_order.project_number',
+                )
+            )
+            ->add(
+                'customer',
+                null,
+                array(
+                    'label' => 'admin.windfarm.customer',
+                    'sortable' => true,
+                    'sort_field_mapping' => array('fieldName' => 'name'),
+                    'sort_parent_association_mappings' => array(array('fieldName' => 'customer')),
+                )
+            )
+            ->add('isFromAudit',
+                null,
+                array(
+                    'label' => 'admin.work_order.is_from_audit',
+                )
+            )
+            ->add('certifyingCompanyName',
+                null,
+                array(
+                    'label' => 'admin.work_order.certifying_company_name',
+                )
+            )
+            ->add('certifyingCompanyContactPerson',
+                null,
+                array(
+                    'label' => 'admin.work_order.certifying_company_contact_person',
+                )
+            )
+            ->add('certifyingCompanyPhone',
+                null,
+                array(
+                    'label' => 'admin.work_order.certifying_company_phone',
+                )
+            )
+            ->add('certifyingCompanyEmail',
+                null,
+                array(
+                    'label' => 'admin.work_order.certifying_company_email',
+                )
+            )
+            ->add('repairAccessTypes',
+                null,
+                array(
+                    'label' => 'admin.work_order.repair_access_types',
+                )
+            )
+            ->add(
+                '_action',
+                'actions',
+                array(
+                    'label' => 'admin.common.action',
+                    'actions' => array(
+                        'edit' => array('template' => '::Admin/Buttons/list__action_edit_button.html.twig'),
+                        'show' => array('template' => '::Admin/Buttons/list__action_show_button.html.twig'),
+ //                       'excel' => array('template' => '::Admin/Buttons/list__action_excel_button.html.twig'),
+ //                       'pdf' => array('template' => '::Admin/Buttons/list__action_pdf_windfarm_button.html.twig'),
+                    ),
+                )
+            )
         ;
     }
 
