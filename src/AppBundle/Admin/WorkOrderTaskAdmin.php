cc1640a32303ff8d03b58162914acd0ec71fae75
jordi.sort <jordi.sort@mirmit.com>
2019-09-02 19:10:59 +0200

    WorkOrderTaskAdmin created

---
+++ src/AppBundle/Admin/WorkOrderTaskAdmin.php
@@ -0,0 +1,376 @@
+<?php
+
+namespace AppBundle\Admin;
+
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
+
+class WorkOrderTaskAdmin extends AbstractBaseAdmin
+{
+    protected $classnameLabel = 'admin.workordertask.title';
+    protected $baseRoutePattern = 'workorders/workorder/workordertask';
+    protected $datagridValues = array(
+        '_sort_by' => 'id',
+        '_sort_order' => 'desc',
+    );
+
+    /**
+     * @param DatagridMapper $datagridMapper
+     */
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add(
+                'workOrder',
+                null,
+                array(
+                    'label' => 'admin.workorder.title',
+                    'sortable' => true,
+                    'sort_field_mapping' => array('fieldName' => 'projectNumber'),
+                    'sort_parent_association_mappings' => array(array('fieldName' => 'workOrder')),
+                )
+            )
+            ->add('isFromAudit',
+                null,
+                array(
+                    'label' => 'admin.workorder.is_from_audit',
+                )
+            )
+            ->add('windmillBlade',
+                null,
+                array(
+                    'label' => 'admin.windmillblade.title',
+                )
+            )
+            ->add('windmill',
+                null,
+                array(
+                    'label' => 'admin.windmill.title',
+                )
+            )
+            ->add('bladeDamage',
+                null,
+                array(
+                    'label' => 'admin.bladedamage.title',
+                )
+            )
+            ->add(
+                'position',
+                null,
+                array(
+                    'label' => 'admin.bladedamage.position',
+                )
+            )
+            ->add(
+                'radius',
+                null,
+                array(
+                    'label' => 'admin.bladedamage.radius',
+                )
+            )
+            ->add(
+                'distance',
+                null,
+                array(
+                    'label' => 'admin.bladedamage.distance',
+                )
+            )
+            ->add(
+                'size',
+                null,
+                array(
+                    'label' => 'admin.bladedamage.size',
+                )
+            )
+            ->add('isCompleted',
+                null,
+                array(
+                    'label' => 'admin.workordertask.is_completed',
+                )
+            )
+        ;
+    }
+
+    /**
+     * @param ListMapper $listMapper
+     */
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->add(
+                'workOrder',
+                null,
+                array(
+                    'label' => 'admin.workorder.title',
+                    'sortable' => true,
+                    'sort_field_mapping' => array('fieldName' => 'projectNumber'),
+                    'sort_parent_association_mappings' => array(array('fieldName' => 'workOrder')),
+                )
+            )
+            ->add('isFromAudit',
+                null,
+                array(
+                    'label' => 'admin.workorder.is_from_audit',
+                )
+            )
+            ->add('windmillBlade',
+                null,
+                array(
+                    'label' => 'admin.windmillblade.title',
+                )
+            )
+            ->add('windmill',
+                null,
+                array(
+                    'label' => 'admin.windmill.title',
+                )
+            )
+            ->add('bladeDamage',
+                null,
+                array(
+                    'label' => 'admin.bladedamage.title',
+                )
+            )
+            ->add(
+                'position',
+                null,
+                array(
+                    'label' => 'admin.bladedamage.position',
+                )
+            )
+            ->add(
+                'radius',
+                null,
+                array(
+                    'label' => 'admin.bladedamage.radius',
+                )
+            )
+            ->add(
+                'distance',
+                null,
+                array(
+                    'label' => 'admin.bladedamage.distance',
+                )
+            )
+            ->add(
+                'size',
+                null,
+                array(
+                    'label' => 'admin.bladedamage.size',
+                )
+            )
+            ->add('isCompleted',
+                null,
+                array(
+                    'label' => 'admin.workordertask.is_completed',
+                )
+            )
+            ->add(
+                '_action',
+                'actions',
+                array(
+                    'label' => 'admin.common.action',
+                    'actions' => array(
+                        'edit' => array('template' => '::Admin/Buttons/list__action_edit_button.html.twig'),
+                        'show' => array('template' => '::Admin/Buttons/list__action_show_button.html.twig'),
+ //                       'excel' => array('template' => '::Admin/Buttons/list__action_excel_button.html.twig'),
+ //                       'pdf' => array('template' => '::Admin/Buttons/list__action_pdf_windfarm_button.html.twig'),
+                    ),
+                )
+            )
+        ;
+    }
+
+    /**
+     * @param FormMapper $formMapper
+     */
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->with('admin.workordertask.title')
+            ->add(
+                'workOrder',
+                null,
+                array(
+                    'label' => 'admin.workorder.title',
+                    'sortable' => true,
+                    'sort_field_mapping' => array('fieldName' => 'projectNumber'),
+                    'sort_parent_association_mappings' => array(array('fieldName' => 'workOrder')),
+                )
+            )
+            ->add('description',
+                null,
+                array(
+                    'label' => 'admin.workordertask.description',
+                )
+            )
+            ->add('isFromAudit',
+                null,
+                array(
+                    'label' => 'admin.workorder.is_from_audit',
+                )
+            )
+            ->add('windmillBlade',
+                ModelType::class,
+                array(
+                    'label' => 'admin.windmillblade.title',
+                    'btn_add' => false,
+                    'required' => true,
+                    //'query' => $this->w->findEnabledSortedByNameQ(),
+                    'choices_as_values' => true,
+                )
+            )
+            ->add('windmill',
+                ModelType::class,
+                array(
+                    'label' => 'admin.windmill.title',
+                    'btn_add' => false,
+                    'required' => true,
+                    'query' => $this->wmr->findCustomerSortedByCustomerWindfarmAndWindmillCode(),
+                    'choices_as_values' => true,
+                )
+            )
+            ->add('isCompleted',
+                null,
+                array(
+                    'label' => 'admin.workordertask.is_completed',
+                )
+            )
+            ->add(
+                'bladeDamage',
+                ModelType::class,
+                array(
+                    'label' => 'admin.bladedamage.title',
+                    'btn_add' => false,
+                    'required' => true,
+                    //'query' => $this->wfr->findEnabledSortedByNameQ(),
+                    'choices_as_values' => true,
+                )
+            )
+            ->add(
+                'position',
+                null,
+                array(
+                    'label' => 'admin.bladedamage.position',
+                )
+            )
+            ->add(
+                'radius',
+                null,
+                array(
+                    'label' => 'admin.bladedamage.radius',
+                )
+            )
+            ->add(
+                'distance',
+                null,
+                array(
+                    'label' => 'admin.bladedamage.distance',
+                )
+            )
+            ->add(
+                'size',
+                null,
+                array(
+                    'label' => 'admin.bladedamage.size',
+                )
+            )
+            ->end()
+        ;
+    }
+
+    protected function configureShowFields(ShowMapper $showMapper)
+    {
+        $showMapper
+            ->add(
+                'workOrder',
+                null,
+                array(
+                    'label' => 'admin.workorder.title',
+                    'sortable' => true,
+                    'sort_field_mapping' => array('fieldName' => 'projectNumber'),
+                    'sort_parent_association_mappings' => array(array('fieldName' => 'workOrder')),
+                )
+            )
+            ->add('description',
+                null,
+                array(
+                    'label' => 'admin.workordertask.description',
+                )
+            )
+            ->add('isFromAudit',
+                null,
+                array(
+                    'label' => 'admin.workorder.is_from_audit',
+                )
+            )
+            ->add('windmillBlade',
+                ModelType::class,
+                array(
+                    'label' => 'admin.windmillblade.title',
+                    'btn_add' => false,
+                    'required' => true,
+                    //'query' => $this->w->findEnabledSortedByNameQ(),
+                    'choices_as_values' => true,
+                )
+            )
+            ->add('windmill',
+                ModelType::class,
+                array(
+                    'label' => 'admin.windmill.title',
+                    'btn_add' => false,
+                    'required' => true,
+                    'query' => $this->wmr->findCustomerSortedByCustomerWindfarmAndWindmillCode(),
+                    'choices_as_values' => true,
+                )
+            )
+            ->add('isCompleted',
+                null,
+                array(
+                    'label' => 'admin.workordertask.is_completed',
+                )
+            )
+            ->add(
+                'bladeDamage',
+                ModelType::class,
+                array(
+                    'label' => 'admin.bladedamage.title',
+                    'btn_add' => false,
+                    'required' => true,
+                    //'query' => $this->wfr->findEnabledSortedByNameQ(),
+                    'choices_as_values' => true,
+                )
+            )
+            ->add(
+                'position',
+                null,
+                array(
+                    'label' => 'admin.bladedamage.position',
+                )
+            )
+            ->add(
+                'radius',
+                null,
+                array(
+                    'label' => 'admin.bladedamage.radius',
+                )
+            )
+            ->add(
+                'distance',
+                null,
+                array(
+                    'label' => 'admin.bladedamage.distance',
+                )
+            )
+            ->add(
+                'size',
+                null,
+                array(
+                    'label' => 'admin.bladedamage.size',
+                )
+            )
+        ;
+    }
+}
