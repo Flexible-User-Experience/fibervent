3c496aa549ff9bd2b7e694928d50d260affd0152
jordi.sort <jordi.sort@mirmit.com>
2019-09-04 10:49:04 +0200

    NonStandardUSedMaterial entity created

---
+++ src/AppBundle/Entity/NonStandardUsedMaterial.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use AppBundle\Enum\NonStandardUsedMaterialItemEnum;
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
+
+/**
+ * NonStandardUsedMaterial.
+ *
+ * @category Entity
+ *
+ * @author   Jordi Sort <jordi.sort@mirmit.com>
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\NonStandardUsedMaterialRepository")
+ * @Gedmo\SoftDeleteable(fieldName="removedAt", timeAware=false)
+ */
+class NonStandardUsedMaterial extends AbstractBase
+{
+    /**
+     * @var float
+     *
+     * @ORM\Column(type="float")
+     */
+    private $quantity;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(type="integer")
+     */
+    private $item;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", length=100, nullable=true)
+     */
+    private $description;
+
+    /**
+     * Methods.
+     */
+
+    /**
+     * @return float
+     */
+    public function getQuantity(): float
+    {
+        return $this->quantity;
+    }
+
+    /**
+     * @param float $quantity
+     *
+     * @return NonStandardUsedMaterial
+     */
+    public function setQuantity(float $quantity): NonStandardUsedMaterial
+    {
+        $this->quantity = $quantity;
+
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getItem(): int
+    {
+        return $this->item;
+    }
+
+    /**
+     * @return string
+     */
+    public function getItemString()
+    {
+        return NonStandardUsedMaterialItemEnum::getStringValue($this);
+    }
+
+    /**
+     * @param int $item
+     *
+     * @return NonStandardUsedMaterial
+     */
+    public function setItem(int $item): NonStandardUsedMaterial
+    {
+        $this->item = $item;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string $description
+     *
+     * @return NonStandardUsedMaterial
+     */
+    public function setDescription(string $description): NonStandardUsedMaterial
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+}
