301b977e1e9340b44fcae423c4a5e83f4bdb1913
jordi.sort <jordi.sort@mirmit.com>
2019-09-04 10:50:54 +0200

    NonStandardUSedMaterial repository created

---
+++ src/AppBundle/Repository/NonStandardUsedMaterialRepository.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace AppBundle\Repository;
+
+use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\Query;
+use Doctrine\ORM\QueryBuilder;
+
+/**
+ * Class NonStandardUsedMaterialRepository.
+ *
+ * @category Repository
+ *
+ * @author   Jordi Sort <jordi.sort@mirmit.com>
+ */
+class NonStandardUsedMaterialRepository extends EntityRepository
+{
+    /**
+     * @param null   $limit
+     * @param string $order
+     *
+     * @return QueryBuilder
+     */
+    public function findAllSortedByProjectNumberQB($limit = null, $order = 'ASC')
+    {
+        $query = $this
+            ->createQueryBuilder('t')
+            ->orderBy('t.id', $order);
+
+        if (!is_null($limit)) {
+            $query->setMaxResults($limit);
+        }
+
+        return $query;
+    }
+
+    /**
+     * @param null   $limit
+     * @param string $order
+     *
+     * @return Query
+     */
+    public function findAllSortedByProjectNumberQ($limit = null, $order = 'ASC')
+    {
+        return $this->findAllSortedByProjectNumberQB($limit, $order)->getQuery();
+    }
+
+    /**
+     * @param null   $limit
+     * @param string $order
+     *
+     * @return array
+     */
+    public function findAllSortedByName($limit = null, $order = 'ASC')
+    {
+        return $this->findAllSortedByProjectNumberQ($limit, $order)->getResult();
+    }
+}
