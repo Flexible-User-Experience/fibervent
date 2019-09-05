39c60504d95f18bc287dceb2780ebef0988ac2c2
jordi.sort <jordi.sort@mirmit.com>
2019-09-05 11:02:27 +0200

    PresenceMonitoring repository created

---
+++ src/AppBundle/Repository/PresenceMonitoringRepository.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace AppBundle\Repository;
+
+use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\Query;
+use Doctrine\ORM\QueryBuilder;
+
+/**
+ * Class PresenceMonitoring.
+ *
+ * @category Repository
+ *
+ * @author   Jordi Sort <jordi.sort@mirmit.com>
+ */
+class PresenceMonitoring extends EntityRepository
+{
+    /**
+     * @param null   $limit
+     * @param string $order
+     *
+     * @return QueryBuilder
+     */
+    public function findAllSortedByIdQB($limit = null, $order = 'ASC')
+    {
+        $query = $this
+            ->createQueryBuilder('t')
+            ->orderBy('t.id', $order);
+
+        if (!is_null($limit)) {
+            $query->setMaxResults($limit);
+        }
+
+        return $query;
+    }
+
+    /**
+     * @param null   $limit
+     * @param string $order
+     *
+     * @return Query
+     */
+    public function findAllSortedByIdQ($limit = null, $order = 'ASC')
+    {
+        return $this->findAllSortedByIdQB($limit, $order)->getQuery();
+    }
+
+    /**
+     * @param null   $limit
+     * @param string $order
+     *
+     * @return array
+     */
+    public function findAllSortedById($limit = null, $order = 'ASC')
+    {
+        return $this->findAllSortedByIdQ($limit, $order)->getResult();
+    }
+}
