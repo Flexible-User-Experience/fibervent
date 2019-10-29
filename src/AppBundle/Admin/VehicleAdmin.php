1cfb1ef67d1a5b24791dd7a28fec0ab1476b45d3
David Romaní <david@flux.cat>
2019-10-29 16:37:57 +0100

    unset mosaic list view && remove unnecessary show view

--- src/AppBundle/Admin/VehicleAdmin.php
+++ src/AppBundle/Admin/VehicleAdmin.php
@@ -5,8 +5,6 @@ namespace AppBundle\Admin;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Form\FormMapper;
-use Sonata\AdminBundle\Route\RouteCollection;
-use Sonata\AdminBundle\Show\ShowMapper;
 
 /**
  * Class VehicleAdmin.
@@ -23,14 +21,6 @@ class VehicleAdmin extends AbstractBaseAdmin
         '_sort_by' => 'id',
         '_sort_order' => 'desc',
     );
-
-    /**
-     * @param RouteCollection $collection
-     */
-    protected function configureRoutes(RouteCollection $collection)
-    {
-        $collection->remove('batch');
-    }
 
     /**
      * @param DatagridMapper $datagridMapper
@@ -65,6 +55,7 @@ class VehicleAdmin extends AbstractBaseAdmin
      */
     protected function configureListFields(ListMapper $listMapper)
     {
+        unset($this->listModes['mosaic']);
         $listMapper
             ->add(
                 'name',
@@ -92,9 +83,6 @@ class VehicleAdmin extends AbstractBaseAdmin
                     'label' => 'admin.common.action',
                     'actions' => array(
                         'edit' => array('template' => '::Admin/Buttons/list__action_edit_button.html.twig'),
-                        'show' => array('template' => '::Admin/Buttons/list__action_show_button.html.twig'),
-                        //                       'excel' => array('template' => '::Admin/Buttons/list__action_excel_button.html.twig'),
-                        //                       'pdf' => array('template' => '::Admin/Buttons/list__action_pdf_windfarm_button.html.twig'),
                     ),
                 )
             )
@@ -108,36 +96,6 @@ class VehicleAdmin extends AbstractBaseAdmin
     {
         $formMapper
             ->with('admin.vehicle.title', $this->getFormMdSuccessBoxArray(4))
-            ->add(
-                'name',
-                null,
-                array(
-                    'label' => 'admin.vehicle.name',
-                )
-            )
-            ->add('licensePlate',
-                null,
-                array(
-                    'label' => 'admin.vehicle.licence_plate',
-                )
-            )
-            ->add('active',
-                null,
-                array(
-                    'label' => 'admin.vehicle.active',
-                )
-            )
-            ->end()
-        ;
-    }
-
-    /**
-     * @param ShowMapper $showMapper
-     */
-    protected function configureShowFields(ShowMapper $showMapper)
-    {
-        $showMapper
-            ->with('admin.vehicle.title', $this->getFormMdSuccessBoxArray(3))
             ->add(
                 'name',
                 null,
